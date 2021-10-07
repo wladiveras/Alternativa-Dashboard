b4deedc90812d34aa0b9c1236b7fcdbf1f91d11c
wladiveras <wladinart@gmail.com>
2021-10-07 17:49:57 -0300

    Deplyo Tester

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,7 +32,7 @@
 <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
 
 <head>
-    <title>{{ env('APP_NAME') }} | {{ $title }}</title>
+    <title> Deploying Test - {{ env('APP_NAME') }} | {{ $title }}</title>
 
     <meta charset="utf-8" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
