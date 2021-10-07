49d599281e0b4fe7a7fa9878169e5c47c3c68387
wladiveras <wladinart@gmail.com>
2021-10-07 16:05:15 -0300

    Sync Update

---
+++ resources/views/cards/components/card13__back_modal.blade.php
@@ -0,0 +1,23 @@
+    <!-- Dados do Verso do cartão -->
+
+    <h4>Dados do <b>verso</b> do Cartão</h4>
+    <br>
+    
+      <div class="form-group">
+        <label>Pai <span class="text-danger">*</span></label>
+        <input type="text" name="back-input-0" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo <b>Pai</b> é necessário.</div>
+     </div>
+    
+      <div class="form-group">
+         <label>Mãe <span class="text-danger">*</span></label>
+         <input type="text" name="back-input-1" class="form-control" maxlength="50" required>
+         <div class="invalid-feedback">O campo <b>Mãe</b> é necessário.</div>
+      </div>
+
+     <div class="form-group">
+         <label>Data de Expedição <span class="text-danger">*</span></label>
+         <input type="text" name="back-input-2" class="form-control" maxlength="50" required>
+         <div class="invalid-feedback">O campo <b>Data de Expedição</b> é necessário.</div>
+      </div>
+   
