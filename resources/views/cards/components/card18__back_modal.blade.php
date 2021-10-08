1a4cfded2fbb830a43338dd2140c1a59d0870fca
wladiveras <wladinart@gmail.com>
2021-10-08 10:31:45 -0300

    Add New Cards

---
+++ resources/views/cards/components/card18__back_modal.blade.php
@@ -0,0 +1,27 @@
+    <!-- Dados do Verso do cartão -->
+
+    <h4>Dados do <b>verso</b> do Cartão</h4>
+    <br>
+    <div class="form-group">
+        <label>CPF <span class="text-danger">*</span></label>
+        <input type="number" name="back-input-0" class="form-control" min="1" max="99999999999" required>
+        <div class="invalid-feedback">O campo <b>CPF</b> é necessário.</div>
+    </div>
+
+    <div class="form-group">
+        <label>RG <span class="text-danger">*</span></label>
+        <input type="text" name="back-input-1" class="form-control" maxlength="21" required>
+        <div class="invalid-feedback">O campo <b>RG</b> é necessário.</div>
+     </div>
+     
+     <div class="form-group">
+        <label>Data de Nascimento <span class="text-danger">*</span></label>
+        <input type="text" name="back-input-2" class="form-control" maxlength="21" required>
+        <div class="invalid-feedback">O campo <b>Data de Nascimento</b> é necessário.</div>
+     </div>
+
+     <div class="form-group">
+        <label>Matricula <span class="text-danger">*</span></label>
+        <input type="text" name="back-input-3" class="form-control" maxlength="21" required>
+        <div class="invalid-feedback">O campo <b>Matricula</b> é necessário.</div>
+     </div>
