b9deaae9403320ba328c12447fa65f77dcc1ff87
wladiveras <wladinart@gmail.com>
2021-10-18 10:28:44 -0300

    Add card/Fonts

---
+++ resources/views/cards/components/card23__back_modal.blade.php
@@ -0,0 +1,15 @@
+    <!-- Dados do Verso do cartão -->
+
+    <h4>Dados do <b>verso</b> do Cartão</h4>
+    <br>
+    <div class="form-group">
+        <label>Nome completo <span class="text-danger">*</span></label>
+        <input type="text" name="back-input-0" class="form-control" maxlength="51" required>
+        <div class="invalid-feedback">O campo <b>Nome completo</b> é necessário.</div>
+    </div>
+
+    <div class="form-group">
+        <label>CPF <span class="text-danger">*</span></label>
+        <input type="text" name="back-input-1" class="form-control" maxlength="21" required>
+        <div class="invalid-feedback">O campo <b>CPF</b> é necessário.</div>
+     </div>
---
+++ resources/views/cards/components/card24__back_modal.blade.php
@@ -0,0 +1,15 @@
+    <!-- Dados do Verso do cartão -->
+
+    <h4>Dados do <b>verso</b> do Cartão</h4>
+    <br>
+    <div class="form-group">
+        <label>RG <span class="text-danger">*</span></label>
+        <input type="text" name="back-input-0" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo <b>RG</b> é necessário.</div>
+    </div>
+
+    <div class="form-group">
+        <label>Tipo Sanguíneo <span class="text-danger">*</span></label>
+        <input type="text" name="back-input-1" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo <b>Tipo Sanguíneo</b> é necessário.</div>
+     </div>
