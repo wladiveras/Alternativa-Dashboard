4c4847a9067e6c554e3070e2b7441f81b82be23a
wladiveras <wladinart@gmail.com>
2021-10-19 16:09:39 -0300

    New Cards

---
+++ resources/views/cards/components/card28__back_modal.blade.php
@@ -0,0 +1,53 @@
+    <!-- Dados do Verso do cartão -->
+
+    <h4>Dados do <b>verso</b> do Cartão</h4>
+    <br>
+    <div class="form-group">
+        <label>RG<span class="text-danger">*</span></label>
+        <input type="text" name="back-input-0" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo <b>RG</b> é necessário.</div>
+    </div>
+
+    <div class="form-group">
+        <label>CPF<span class="text-danger">*</span></label>
+        <input type="text" name="back-input-1" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo <b>CPF</b> é necessário.</div>
+     </div>
+
+     <div class="form-group">
+        <label>Nome do Pai<span class="text-danger">*</span></label>
+        <input type="text" name="back-input-2" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo <b>Nome do Pai</b> é necessário.</div>
+     </div>
+
+     <div class="form-group">
+        <label>Nome da Mãe<span class="text-danger">*</span></label>
+        <input type="text" name="back-input-3" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo <b>Nome da Mãe</b> é necessário.</div>
+     </div>
+
+     <div class="form-group">
+         <label>Estado Civil<span class="text-danger">*</span></label>
+         <input type="text" name="back-input-4" class="form-control" maxlength="50" required>
+         <div class="invalid-feedback">O campo <b>Estado Civil</b> é necessário.</div>
+      </div>
+
+      <div class="form-group">
+         <label>Naturalidade<span class="text-danger">*</span></label>
+         <input type="text" name="back-input-5" class="form-control" maxlength="50" required>
+         <div class="invalid-feedback">O campo <b>Naturalidade</b> é necessário.</div>
+      </div>
+   
+      <div class="form-group">
+         <label>Nacionalidade<span class="text-danger">*</span></label>
+         <input type="text" name="back-input-6" class="form-control" maxlength="50" required>
+         <div class="invalid-feedback">O campo <b>Nacionalidade</b> é necessário.</div>
+      </div>
+
+      <div class="form-group">
+         <label>Data de Consagração<span class="text-danger">*</span></label>
+         <input type="text" name="back-input-7" class="form-control" maxlength="50" required>
+         <div class="invalid-feedback">O campo <b>Data de Consagração</b> é necessário.</div>
+      </div>
+   
+   
