a8f6ae0139c823ee9f8a4486c8fc2c978d887fb4
wladiveras <wladinart@gmail.com>
2021-10-21 13:32:51 -0300

    New Card

---
+++ resources/views/cards/components/card32__back_modal.blade.php
@@ -0,0 +1,30 @@
+    <!-- Dados do Verso do cartão -->
+
+    <h4>Dados do <b>verso</b> do Cartão</h4>
+    <br>
+    <div class="form-group">
+        <label>Nome<span class="text-danger">*</span></label>
+        <input type="text" name="back-input-0" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo <b>Nome</b> é necessário.</div>
+    </div>
+
+    <div class="form-group">
+        <label>RG<span class="text-danger">*</span></label>
+        <input type="text" name="back-input-1" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo <b>RG</b> é necessário.</div>
+     </div>
+
+     <div class="form-group">
+        <label>Função<span class="text-danger">*</span></label>
+        <input type="text" name="back-input-2" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo <b>Função</b> é necessário.</div>
+     </div>
+
+     <div class="form-group">
+        <label>Matricula<span class="text-danger">*</span></label>
+        <input type="text" name="back-input-3" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo <b>Matricula</b> é necessário.</div>
+     </div>
+
+
+    
