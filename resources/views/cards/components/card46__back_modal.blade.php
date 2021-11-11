f22468203ef53ba995c85995a775922978f27139
Wladi Veras <wladinart@gmail.com>
2021-11-11 16:22:49 -0300

    💄 New Card

---
+++ resources/views/cards/components/card46__back_modal.blade.php
@@ -0,0 +1,13 @@
+    <!-- Dados do Verso do cartão -->
+    <h4>Dados do <b>verso</b> do Cartão</h4>
+    <br>
+    <div class="form-group">
+        <label>Igreja<span class="text-danger">*</span></label>
+        <input type="text" name="back-input-0" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo <b>Igreja</b> é necessário.</div>
+    </div>
+    <div class="form-group">
+        <label>Sanguineo<span class="text-danger">*</span></label>
+        <input type="text" name="back-input-1" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo <b>Sanguineo</b> é necessário.</div>
+    </div>
