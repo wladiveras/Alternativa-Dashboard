085928672db040dfc70e2d2133f9367bca992855
Wladi Veras <wladinart@gmail.com>
2021-11-23 15:09:13 -0300

    💄 New Card

---
+++ resources/views/cards/components/card50__back_modal.blade.php
@@ -0,0 +1,24 @@
+    <!-- Dados do Verso do cartão -->
+    <h4>Dados do <b>verso</b> do Cartão</h4>
+    <br>
+    <div class="form-group">
+        <label>Estado Civil<span class="text-danger">*</span></label>
+        <input type="text" name="back-input-0" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo <b>Estado Civil</b> é necessário.</div>
+    </div>
+    <div class="form-group">
+        <label>Data de Batismo<span class="text-danger">*</span></label>
+        <input type="text" name="back-input-1" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo <b>Data de Batismo</b> é necessário.</div>
+    </div>
+    <div class="form-group">
+        <label>Emissão<span class="text-danger">*</span></label>
+        <input type="text" name="back-input-2" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo <b>Emissão</b> é necessário.</div>
+    </div>
+    <div class="form-group">
+        <label>Validade<span class="text-danger">*</span></label>
+        <input type="text" name="back-input-3" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo <b>Validade</b> é necessário.</div>
+    </div>
+  
