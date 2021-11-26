cd2ad9610435c8b500da558dce17ed47f0ebed33
wladiveras <wladianrt@gmail.com>
2021-11-26 17:48:29 -0300

    💄 New Card

---
+++ resources/views/cards/components/card52__back_modal.blade.php
@@ -0,0 +1,33 @@
+    <!-- Dados do Verso do cartão -->
+    <h4>Dados do <b>verso</b> do Cartão</h4>
+    <br>
+    <div class="form-group">
+        <label>Nome<span class="text-danger">*</span></label>
+        <input type="text" name="back-input-0" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo <b>Nome</b> é necessário.</div>
+    </div>
+    <div class="form-group">
+        <label>Matricula<span class="text-danger">*</span></label>
+        <input type="text" name="back-input-1" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo <b>Matricula<</b> é necessário.</div>
+    </div>
+    <div class="form-group">
+        <label>Admissão<span class="text-danger">*</span></label>
+        <input type="text" name="back-input-2" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo <b>Admissão</b> é necessário.</div>
+    </div>
+    <div class="form-group">
+        <label>Identidade<span class="text-danger">*</span></label>
+        <input type="text" name="back-input-3" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo <b>Identidade</b> é necessário.</div>
+    </div>
+    <div class="form-group">
+        <label>CPF<span class="text-danger">*</span></label>
+        <input type="text" name="back-input-4" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo <b>CPF</b> é necessário.</div>
+    </div>
+    <div class="form-group">
+        <label>PIS<span class="text-danger">*</span></label>
+        <input type="text" name="back-input-5" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo <b>PIS</b> é necessário.</div>
+    </div>
---
+++ resources/views/cards/components/card53__back_modal.blade.php
@@ -0,0 +1,23 @@
+    <!-- Dados do Verso do cartão -->
+    <h4>Dados do <b>verso</b> do Cartão</h4>
+    <br>
+    <div class="form-group">
+        <label>Nome<span class="text-danger">*</span></label>
+        <input type="text" name="back-input-0" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo <b>Nome</b> é necessário.</div>
+    </div>
+    <div class="form-group">
+        <label>CPF<span class="text-danger">*</span></label>
+        <input type="text" name="back-input-1" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo <b>CPF<</b> é necessário.</div>
+    </div>
+    <div class="form-group">
+        <label>ID<span class="text-danger">*</span></label>
+        <input type="text" name="back-input-2" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo <b>ID</b> é necessário.</div>
+    </div>
+    <div class="form-group">
+        <label>Função<span class="text-danger">*</span></label>
+        <input type="text" name="back-input-3" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo <b>Função</b> é necessário.</div>
+    </div>
