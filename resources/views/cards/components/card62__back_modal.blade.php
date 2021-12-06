46b584aaee0a3f8bc426f9136a62a68378c814b6
wladiveras <wladinart@gmail.com>
2021-12-06 16:26:19 -0300

    💄 New Card

---
+++ resources/views/cards/components/card62__back_modal.blade.php
@@ -0,0 +1,41 @@
+    <!-- Dados do Verso do cartão -->
+
+    <h4>Dados do <b>verso</b> do Cartão</h4>
+    <br>
+    <div class="form-group">
+        <label>Nome <span class="text-danger">*</span></label>
+        <input type="text" name="back-input-0" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo é necessário.</div>
+    </div>
+    <div class="form-group">
+        <label>Cargo <span class="text-danger">*</span></label>
+        <input type="text" name="back-input-1" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo é necessário.</div>
+    </div>
+    <div class="form-group">
+        <label>Matricula <span class="text-danger">*</span></label>
+        <input type="text" name="back-input-2" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo é necessário.</div>
+    </div>
+    <div class="form-group">
+        <label>RG <span class="text-danger">*</span></label>
+        <input type="text" name="back-input-3" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo é necessário.</div>
+    </div>
+
+    <div class="form-group">
+        <label>Tipo Sanguineo <span class="text-danger">*</span></label>
+        <input type="text" name="back-input-4" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo é necessário.</div>
+    </div>
+
+    <div class="form-group">
+        <label>Orgão Emissor <span class="text-danger">*</span></label>
+        <input type="text" name="back-input-5" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo é necessário.</div>
+    </div>
+    <div class="form-group">
+      <label>Secretaria <span class="text-danger">*</span></label>
+      <input type="text" name="back-input-6" class="form-control" maxlength="50" required>
+      <div class="invalid-feedback">O campo é necessário.</div>
+  </div>
