1605890ec451d7ba5b2717e96ac8de4bd795cdd9
wladiveras <wladinart@gmail.com>
2021-12-17 17:10:35 -0300

    💄 New Cards

--- resources/views/cards/components/card78__table.blade.php
+++ resources/views/cards/components/card78__table.blade.php
@@ -1,20 +1,31 @@
-<table class="table table-separate table-head-custom table-checkable" id="kt_datatable">
+{{-- <table class="table table-separate table-head-custom table-checkable" id="kt_datatable_clear"> --}}
+<table class="table table-separate table-head-custom table-checkable" id="kt_datatable"> 
     <thead>
         <tr>
             <th>ID</th>
-            <th>Detalhe</th>
+            <th>Detalhe</th>  
+
             <th>Nome</th>
             <th>Função</th>
+            <th>RG</th>
+            <th>CPF</th>
+          
             <th>Ações</th>
         </tr>
     </thead>
+
     <tbody>
         @foreach($order_data as $D)
         <tr>
             <td>{{$D->id}}</td>
-            <td>{{ asset('assets/media/users/'. $order->order_id .'/'.$D->front_avatar)}}</td>
+            <td>{{ asset('assets/media/users/'. $order->order_id .'/'.$D->front_avatar)}}</td> 
+            
             <td>{{ $D->front_input1 }}</td>
             <td>{{ $D->front_input2 }}</td>
+            <td>{{ $D->front_input3 }}</td>
+            <td>{{ $D->front_input4 }}</td>
+      
+
             <td nowrap="nowrap">
                 <form method="POST" action="{{ route('OrderDelelete') }}">
                     @csrf
@@ -26,7 +37,6 @@
                 </form>
             </td>
         </tr>
-        @endforeachasd
-
+        @endforeach
     </tbody>
 </table>
