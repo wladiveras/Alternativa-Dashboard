eb9ebe4bcbfdd036ac186716de3f85b5d34614f0
wladiveras <wladinart@gmail.com>
2022-02-16 11:10:31 -0300

    New Card

---
+++ resources/views/cards/components/card113__back_modal.blade.php
@@ -0,0 +1,34 @@
+    <!-- Dados do Verso do cartão -->
+    <h4>Dados do <b>verso</b> do Cartão</h4>
+    <br>
+    <div class="form-group">
+        <label>Emissão<span class="text-danger">*</span></label>
+        <input type="text" name="back-input-0" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo é necessário.</div>
+    </div>
+    <div class="form-group">
+        <label>Naturalidade<span class="text-danger">*</span></label>
+        <input type="text" name="back-input-1" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo é necessário.</div>
+    </div>
+    <div class="form-group">
+        <label>Batismo<span class="text-danger">*</span></label>
+        <input type="text" name="back-input-2" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo é necessário.</div>
+    </div>
+    <div class="form-group">
+        <label>Aclamação<span class="text-danger">*</span></label>
+        <input type="text" name="back-input-3" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo é necessário.</div>
+    </div>
+
+    <div class="form-group">
+        <label>Nome do Pai<span class="text-danger">*</span></label>
+        <input type="text" name="back-input-4" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo é necessário.</div>
+    </div>
+    <div class="form-group">
+        <label>Nome da Mãe<span class="text-danger">*</span></label>
+        <input type="text" name="back-input-5" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo é necessário.</div>
+    </div>
---
+++ resources/views/cards/components/card114__back_modal.blade.php
@@ -0,0 +1,8 @@
+    <!-- Dados do Verso do cartão -->
+    <h4>Dados do <b>verso</b> do Cartão</h4>
+    <br>
+    <div class="form-group">
+        <label>Emissão<span class="text-danger">*</span></label>
+        <input type="text" name="back-input-0" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo é necessário.</div>
+    </div>
