b7bb0333768aa684a6f008d900ceacd16d2bbc12
wladiveras <wladinart@gmail.com>
2022-02-22 15:10:12 -0300

    New Cards + Hotfix

--- resources/views/cards/components/card125__back_modal.blade.php
+++ resources/views/cards/components/card125__back_modal.blade.php
@@ -2,12 +2,17 @@
     <h4>Dados do <b>verso</b> do Cartão</h4>
     <br>
     <div class="form-group">
-        <label>Nome Completo:<span class="text-danger">*</span></label>
+        <label>Nome<span class="text-danger">*</span></label>
         <input type="text" name="back-input-0" class="form-control" maxlength="50" required>
         <div class="invalid-feedback">O campo é necessário.</div>
     </div>
     <div class="form-group">
-        <label>RG<span class="text-danger">*</span></label>
+        <label>Função<span class="text-danger">*</span></label>
         <input type="text" name="back-input-1" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo é necessário.</div>
+    </div>
+    <div class="form-group">
+        <label>Fator RH<span class="text-danger">*</span></label>
+        <input type="text" name="back-input-2" class="form-control" maxlength="50" required>
         <div class="invalid-feedback">O campo é necessário.</div>
     </div>
