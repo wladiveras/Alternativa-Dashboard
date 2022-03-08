78695c2223145f28feb69d479730631015dc1128
wladi veras <wladinart@gmail.com>
2022-03-08 18:23:45 -0300

    New Cards

---
+++ resources/views/cards/components/card131__back_modal.blade.php
@@ -0,0 +1,32 @@
+    <!-- Dados do Verso do cartão -->
+    <h4>Dados do <b>verso</b> do Cartão</h4>
+    <br>
+    <div class="form-group">
+        <label>Pai<span class="text-danger">*</span></label>
+        <input type="text" name="back-input-0" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo é necessário.</div>
+    </div>
+
+    <div class="form-group">
+        <label>Mãe<span class="text-danger">*</span></label>
+        <input type="text" name="back-input-1" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo é necessário.</div>
+    </div>
+    
+    <div class="form-group">
+        <label>Batismo<span class="text-danger">*</span></label>
+        <input type="text" name="back-input-2" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo é necessário.</div>
+    </div>
+
+    <div class="form-group">
+        <label>Nacionalidade <span class="text-danger">*</span></label>
+        <input type="text" name="back-input-3" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo é necessário.</div>
+    </div>
+
+    <div class="form-group">
+        <label>Dados QR Code<span class="text-danger">*</span></label>
+        <input type="text" name="back-input-4" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo é necessário.</div>
+    </div>
---
+++ resources/views/cards/components/card132__back_modal.blade.php
@@ -0,0 +1,20 @@
+    <!-- Dados do Verso do cartão -->
+    <h4>Dados do <b>verso</b> do Cartão</h4>
+    <br>
+    <div class="form-group">
+        <label>RG<span class="text-danger">*</span></label>
+        <input type="text" name="back-input-0" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo é necessário.</div>
+    </div>
+
+    <div class="form-group">
+        <label>Tipo Sanguineo<span class="text-danger">*</span></label>
+        <input type="text" name="back-input-1" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo é necessário.</div>
+    </div>
+    
+    <div class="form-group">
+        <label>CI<span class="text-danger">*</span></label>
+        <input type="text" name="back-input-2" class="form-control" maxlength="50" required>
+        <div class="invalid-feedback">O campo é necessário.</div>
+    </div>
